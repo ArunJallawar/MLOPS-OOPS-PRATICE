4140734b74001a7aa8606696c45a61dd595cb9e3
ArunJallawar <arunjallawar888@gmail.com>
2026-02-21 07:42:52 +0000

    covered till chatbook proj

--- rough.py
+++ rough.py
@@ -1,7 +1,10 @@
-lst =[1,2,3]
-my_str ="mlops playlist"
-my_int = 10
+#lst =[1,2,3]
+#my_str ="mlops playlist"
+#my_int = 10
 
 #print(type(lst))
-my_str = my_str.capitalize()
-print(my_str)
+#my_str = my_str.capitalize()
+#print(my_str)
+
+from oops_proj import chatbook
+user1=chatbook()
